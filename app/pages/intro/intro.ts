93db4df3b78aa0364b073abe8a6c22f79fd33cd9
Niño Tan Cardoso <niny0@msn.com>
2016-07-16 01:19:17 +0800

    Applied temporary fix for ionic-slides pager bug on beta10

--- app/pages/intro/intro.ts
+++ app/pages/intro/intro.ts
@@ -1,4 +1,4 @@
-import {Component, ViewChild} from '@angular/core';
+import {Component, ViewChild, ElementRef, OnInit} from '@angular/core';
 import {NavController, Modal, Slides} from 'ionic-angular';
 import {LoginPage} from '../login/login';
 import {RegisterPage} from '../register/register';
@@ -10,23 +10,51 @@ import {TabsPage} from '../tabs/tabs';
 export class IntroPage {
   @ViewChild('introSlider') slider: Slides;
 
-  constructor(private navController: NavController) {
+  constructor(private navController: NavController,
+    private _elementRef: ElementRef) {
 
   }
+
+  /**
+   *  Temporary fix for beta10 slider bug
+   *  https://github.com/GerritErpenstein/ionic2-slides-temp-fix
+   */
+  public ngOnInit() {
+    let swiperContainer = this._elementRef.nativeElement.getElementsByClassName('swiper-container')[0];
+    this.waitRendered(swiperContainer).then(() => {
+      let swiper = this.slider.getSlider();
+      swiper.update();
+    });
+  }
+
+  waitRendered(element: HTMLElement): Promise<HTMLElement> {
+    return new Promise((resolve) => {
+      let checkNextFrame = () => {
+        requestAnimationFrame(() => {
+          if (element.clientWidth)
+            resolve(element);
+          else
+            checkNextFrame();
+        });
+      };
+      checkNextFrame();
+    });
+  }
+
 
   loginPage() {
     let modalPage = Modal.create(LoginPage);
     modalPage.onDismiss(
       data => {
         console.log(data);
-        
-        if(data.success==true) {
+
+        if (data.success == true) {
           console.log("data true");
           this.navController.setRoot(TabsPage);
         } else {
           console.log("data not true");
         }
-    });
+      });
     this.navController.present(modalPage);
   }
 
